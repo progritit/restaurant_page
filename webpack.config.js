f9094bcc522bcb4c39a4e47f07c69851336ad75c
Clebson Costa <210671235+progritit@users.noreply.github.com>
2026-08-07 02:07:57 +0200

    chore: initialize project and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+
+    entry: "./src/index.js",
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        assetModuleFilename: "assets/[name][ext]",
+    },
+
+    devtool: "inline-source-map",
+
+    devServer: {
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset/resource",
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
